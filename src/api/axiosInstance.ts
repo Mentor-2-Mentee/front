91c2fff3c01a6fe78d93df074a01eefa8a8fa18f
정경훈 <ruderud0055@gmail.com>
2022-06-13 09:29:11 +0900

    test : 환경변수값 주입 테스트

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 export const axiosInstance = (config?: AxiosRequestConfig): AxiosInstance => {
   const createInstance = axios.create({
-    baseURL: import.meta.env.VITE_APP_BASEURL,
+    baseURL: `${import.meta.env.VITE_APP_BASEURL}`,
     timeout: 5 * 1000,
     ...config,
     headers: {
