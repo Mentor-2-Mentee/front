f87aa3deb480670a70a66029f2c1258741b4179b
정경훈 <ruderud0055@gmail.com>
2022-06-21 14:23:05 +0900

    fix : socket port fix

--- src/api/socketInstance.ts
+++ src/api/socketInstance.ts
@@ -1,7 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-// const PORT = 443;
-const PORT = 80;
+const PORT = 443;
 export const socketInstance = (): Socket | undefined => {
   try {
     const connectedSocket = io(
