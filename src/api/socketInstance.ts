b8cd382ebdf086d45e4ca5b9f865d8558aecfac9
정경훈 <ruderud0055@gmail.com>
2022-06-25 00:50:01 +0900

    test : 소켓 연결 테스트

--- src/api/socketInstance.ts
+++ src/api/socketInstance.ts
@@ -3,8 +3,9 @@ import { io, Socket } from "socket.io-client";
 export const socketInstance = (): Socket | undefined => {
   try {
     const connectedSocket = io(
-      `${import.meta.env.VITE_APP_SOCKETURL}/live-chat`,
-      // "ws://localhost:443/chats",
+      `${import.meta.env.VITE_APP_SOCKETURL}:8083/live-chat`,
+      // "wss://api.rudbeckiaz.com/live-chat",
+      // "ws://localhost:8081/live-chat",
       {
         transports: ["websocket"],
       }
