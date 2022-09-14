6ec3048c4cd89fe889fae6bb37d5f860a9c01d7f
정경훈 <ruderud0055@gmail.com>
2022-09-14 20:41:27 +0900

    feat : pdf 문제 / 답 각각 pdf로 다운로드 (사진미적용)

--- src/pages/ExamMentoringRoomPage/Components/PdfDownload.tsx
+++ src/pages/ExamMentoringRoomPage/Components/PdfDownload.tsx
@@ -1,8 +1,35 @@
+import { Box, Typography } from "@mui/material";
+import { useParams } from "react-router";
+
 export const PdfDownload = () => {
+  const { examScheduleId, examField } = useParams();
+
   return (
-    <>
-      <div>pdf 다운로드 </div>
-    </>
+    <Box
+      sx={(theme) => ({
+        p: 2,
+        overflow: "scroll",
+        height: `calc((var(--vh, 1vh) * 100) - ${theme.spacing(19)} )`,
+      })}
+    >
+      <Typography variant="h5">PDF 다운로드</Typography>
+      <div>
+        <a
+          href={`${
+            import.meta.env.VITE_APP_BASEURL
+          }/exam-mentoring-room/question-pdf?examScheduleId=${examScheduleId}&examField=${examField}`}
+        >
+          문제 다운로드
+        </a>
+      </div>
+      <a
+        href={`${
+          import.meta.env.VITE_APP_BASEURL
+        }/exam-mentoring-room/solution-pdf?examScheduleId=${examScheduleId}&examField=${examField}`}
+      >
+        솔루션 다운로드
+      </a>
+    </Box>
   );
 };
 
