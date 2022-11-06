7fdaf96955b4c48c196122b62e5910d8dc6b2215
정경훈 <ruderud0055@gmail.com>
2022-11-06 10:34:44 +0900

    fix : 모델파일명 대소문자 변경위한 삭제

--- src/models/types/EnqueueSnackbar.ts
+++
@@ -1,6 +0,0 @@
-import { OptionsObject, SnackbarKey, SnackbarMessage } from "notistack";
-
-export type EnqueueSnackbar = (
-  message: SnackbarMessage,
-  options?: OptionsObject | undefined
-) => SnackbarKey;
